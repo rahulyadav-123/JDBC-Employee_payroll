4f95773ade3444da82dd7c1c4a1c87dc89a264c3
rahul yadav <ry219613@gmail.com>
2021-11-19 00:08:32 +0530

    Retrive employing by joninig date

--- com/jdbc/employee_payroll/Employee_payrollJDBC.java
+++ com/jdbc/employee_payroll/Employee_payrollJDBC.java
@@ -3,20 +3,46 @@ package com.jdbc.employee_payroll;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Employee_payrollJDBC {
 	
-	public static void main(String[] args) throws Exception {
-		String query= "update payroll_service.employee_payroll set salary =30000 where empName ='Charlie'";
-		Class.forName("com.mysql.jdbc.Driver");
-		System.out.println("Driver loaded");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user = root & password = Rahulyadav@123");
-		System.out.println("connection established");
-		PreparedStatement statement = con.prepareStatement(query);
-		System.out.println("platform created");
-		int count = statement.executeUpdate(query);
-		System.out.println(count + " row/s affected");
-		statement.close();
-		con.close();
+	public static void main(String[] args)  {
+		Connection connection = null;
+		Statement statement = null;
+		String query= "select * from payroll_service.employee_payroll where startDate between cast('2018-01-03' as date) and date(now())";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user = root & password = Rahulyadav@123");
+			System.out.println("connection established");
+			statement = con.createStatement();
+			System.out.println("platform created");
+			ResultSet result = statement.executeQuery(query);
+			System.out.println("data fetched");
+            if(result.next()){
+            	 System.out.println("emp_Id = "+result.getInt(1) + " Emp_name = "+result.getString(2) + " Salary =" + result.getInt(3) + " Start Date =" + result.getString(4));
+            }
+		}
+		catch(ClassNotFoundException | SQLException ex){
+			((Throwable) ex).printStackTrace();
+        }
+		finally {
+			if(statement != null){
+                try{
+                    statement.close();
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            } if(connection != null){
+                try{
+                    connection.close();
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+		}
 	}
 }
