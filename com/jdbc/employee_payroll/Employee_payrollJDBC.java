80d68ff7233d66802e4702b0a844c279aaad65a6
rahul yadav <ry219613@gmail.com>
2021-11-19 00:13:15 +0530

    calculated avg, sum, count ,min, max

--- com/jdbc/employee_payroll/Employee_payrollJDBC.java
+++ com/jdbc/employee_payroll/Employee_payrollJDBC.java
@@ -8,11 +8,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Employee_payrollJDBC {
-	
 	public static void main(String[] args)  {
 		Connection connection = null;
 		Statement statement = null;
-		String query= "select * from payroll_service.employee_payroll where startDate between cast('2018-01-03' as date) and date(now())";
+		String query1 = "SELECT SUM(Salary) from payroll_service.employee_payroll WHERE gender = 'F' GROUP BY gender;";
+		String query2 = "SELECT SUM(Salary) from payroll_service.employee_payroll WHERE gender = 'M' GROUP BY gender;";
+		String query3 = "SELECT AVG(Salary) from payroll_service.employee_payroll WHERE gender = 'F' GROUP BY gender;";
+		String query4 = "SELECT AVG(Salary) from payroll_service.employee_payroll WHERE gender = 'M' GROUP BY gender;";
+		String query5 = "SELECT MIN(Salary) from payroll_service.employee_payroll";
+		String query6 = "SELECT MAX(Salary) from payroll_service.employee_payroll";
+		String query7 = "SELECT COUNT(Salary) from payroll_service.employee_payroll";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver loaded");
@@ -20,15 +25,45 @@ public class Employee_payrollJDBC {
 			System.out.println("connection established");
 			statement = con.createStatement();
 			System.out.println("platform created");
-			ResultSet result = statement.executeQuery(query);
-			System.out.println("data fetched");
-            if(result.next()){
-            	 System.out.println("emp_Id = "+result.getInt(1) + " Emp_name = "+result.getString(2) + " Salary =" + result.getInt(3) + " Start Date =" + result.getString(4));
-            }
+			
+			ResultSet result1 = statement.executeQuery(query1);
+			if(result1.next()){
+				System.out.println("Salary =" +result1.getString("SUM(Salary)"));
+				}
+			
+			ResultSet result2 = statement.executeQuery(query2);
+			if(result2.next()){
+				System.out.println("Salary =" +result2.getString("SUM(Salary)"));
+				}
+			
+			ResultSet result3 = statement.executeQuery(query3);
+			if(result3.next()){
+				System.out.println("Salary =" +result3.getString("AVG(Salary)"));
+				}
+			
+			ResultSet result4 = statement.executeQuery(query4);
+			if(result4.next()){
+				System.out.println("Salary =" +result4.getString("AVG(Salary)"));
+				}
+			
+			ResultSet result5 = statement.executeQuery(query5);
+			if(result5.next()){
+				System.out.println("Salary =" +result5.getString("MIN(Salary)"));
+				}
+			
+			ResultSet result6 = statement.executeQuery(query6);
+			if(result6.next()){
+				System.out.println("Salary =" +result6.getString("MAX(Salary)"));
+				}
+			
+			ResultSet result7 = statement.executeQuery(query7);
+			if(result7.next()){
+				System.out.println("Salary =" +result7.getString("COUNT(Salary)"));
+			}
 		}
 		catch(ClassNotFoundException | SQLException ex){
-			((Throwable) ex).printStackTrace();
-        }
+			 ex.printStackTrace();
+		}
 		finally {
 			if(statement != null){
                 try{
@@ -36,7 +71,7 @@ public class Employee_payrollJDBC {
                 }catch(SQLException e){
                     e.printStackTrace();
                 }
-            } if(connection != null){
+			} if(connection != null){
                 try{
                     connection.close();
                 }catch(SQLException e){
